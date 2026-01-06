11f3a4b886344496a42cadf3493f9c1cafc90700
Lucas Nogueira <lucasnogueirachaves@id.uff.br>
2026-01-06 16:19:43 -0300

    Att 2

--- app.py
+++ app.py
@@ -1,171 +1,100 @@
 from flask import Flask, request, jsonify
+from db import jogadores
+from flask_smorest import abort
+import uuid
 
 app = Flask(__name__)
 app.json.sort_keys = False
 
-jogadores = [
-    {
-        "id" : 0,
-        "nome": "Lionel Messi",
-        "idade": 37,
-        "valor": 35000000,
-        "aposentado": False,
-        "time": "Inter Miami"
-    },
-    {
-        "id" : 1,
-        "nome": "Cristiano Ronaldo",
-        "idade": 39,
-        "valor": 15000000,
-        "aposentado": False,
-        "time": "Al-Nassr"
-    },
-    {
-        "id" : 2,
-        "nome": "Neymar Jr",
-        "idade": 32,
-        "valor": 45000000,
-        "aposentado": False,
-        "time": "Santos"
-    },
-    {
-        "id" : 3,
-        "nome": "Ronaldinho Gaúcho",
-        "idade": 44,
-        "valor": 0,
-        "aposentado": True,
-        "time": "Sem clube"
-    },
-    {
-        "id" : 4,
-        "nome": "Pelé",
-        "idade": 82,
-        "valor": 0,
-        "aposentado": True,
-        "time": "Sem clube"
-    }
-]
-
 # GET da lista
 @app.route("/jogadores", methods=['GET'])
 def get_jogadores():
-    return jsonify(Jogadores = jogadores)
+    return jsonify(Jogadores = list(jogadores.values()))
 
 # Busca jogador por id
-@app.route("/jogador/<int:id>", methods=['GET'])
+@app.route("/jogador/<string:id>", methods=['GET'])
 def get_jogador_id(id):
-    for jogador in jogadores:
-        if jogador["id"] == id:
-            return jsonify(Mensagem = "Jogador encontrado com sucesso", Jogador = jogador), 200
-        
-    return jsonify(Mensagem = "Jogador não encontrado"), 404
+    try:
+        return jsonify(jogador = jogadores[id]), 200
+    except:
+        abort(404, message="Jogador não encontrado")
 
 # Busca jogador por nome
 @app.route("/jogador", methods=['GET'])
 def get_jogador_nome():
     nome = request.args.get("nome")
     
-    for jogador in jogadores:
+    for jogador in jogadores.values():
         if jogador["nome"] == nome:
             return jsonify(Mensagem = "Jogador encontrado", Jogador = jogador), 200
         
-    return jsonify(Mensagem = "Jogador não encontrado"), 404
+    abort(404, message="Jogador não encontrado")
 
 # Cadastro de jogador
 @app.route("/jogador", methods=['POST'])
 def cadastrar_jogador():
-    jogador = request.get_json()
+    dado_jogador = request.get_json()
+    jogador_id = uuid.uuid4().hex
     
-    if not jogador:
-        return jsonify(Mensagem = "Erro"), 400
-    if "id" not in jogador:
-        return jsonify(Mensagem = "O campo 'id' é obrigatório"), 400
-    if "nome" not in jogador:
-        return jsonify(Mensagem = "O campo 'nome' é obrigatório"), 400
-    if "aposentado" not in jogador:
-        return jsonify(Mensagem = "O campo 'aposentado' é obrigatório"), 400
+    jogador_novo = {**dado_jogador, "id": jogador_id}
     
-    jogadores.append(jogador)
+    jogadores[jogador_id] = jogador_novo
     
-    return jsonify(Mensagem = "Jogador adicionado", Jogador = jogador), 200
+    return jsonify(Mensagem = "Jogador adicionado"), 201
+    
 
 # Atualizar jogador
-@app.route("/jogador/<int:id>", methods=['PATCH'])
+@app.route("/jogador/<string:id>", methods=['PATCH'])
 def atualizar_jogador(id):
     dado = request.get_json()
     
     if not dado:
-        return jsonify(Mensagem = "Erro"), 400
+        abort(400, message="Dados inválidos")
     
-    for jogador in jogadores:
-        if jogador["id"] == id:
-            if "nome" in dado:
-                jogador["nome"] = dado["nome"]
-            if "idade" in dado:
-                jogador["idade"] = dado["idade"]
-            if "valor" in dado:
-                jogador["valor"] = dado["valor"]
-            if "aposentado" in dado:
-                jogador["aposentado"] = dado["aposentado"]
-            if "time" in dado:
-                jogador["time"] = dado["time"]
-            return jsonify(Mensagem = "Jogador atualizado com sucesso", Jogador = jogador), 200
-    return jsonify(Mensagem = "Jogador não encontrado"), 404
+    jogador = jogadores.get(id)
+
+    if not jogador:
+        abort(404, message="Jogador não encontrado")
+
+    jogador.update(dado)
+    
+    return jsonify(Mensagem = "Jogador atualizado com sucesso", Jogador = jogador), 200
 
 # Atualização completa
-@app.route("/jogador/<int:id>", methods=['PUT'])
-def atualizar(id):
+@app.route("/jogador/<string:id>", methods=['PUT'])
+def atualizar_completo(id):
     dado = request.get_json()
-    if not dado:
-        return jsonify(Mensagem = "Erro"), 400
-    
+
     campos_obrigatorios = ["nome", "valor", "idade", "aposentado", "time"]
-    
+
     for campo in campos_obrigatorios:
         if campo not in dado:
-            return jsonify(Mensagem = f"O campo {campo} é obrigatório"), 400
-        
-    for jogador in jogadores:
-        if jogador["id"] == id:
-            jogador["nome"] = dado["nome"]
-            jogador["idade"] = dado["idade"]
-            jogador["time"] = dado["time"]
-            jogador["valor"] = dado["valor"]
-            jogador["aposentado"] = dado["aposentado"]
-            return jsonify(Mensagem = "Jogador atualizado com sucesso", Jogador = jogador), 200
-        
-    return jsonify(Mensagem = "Jogador não encontrado"), 404
+            abort(400, message=f"Campo {campo} é obrigatório")
 
+    if id not in jogadores:
+        abort(404, message="Jogador não encontrado")
+
+    jogadores[id] = {
+        "id": id,
+        **dado
+    }
+
+    return jsonify(Mensagem="Jogador atualizado com sucesso", Jogador=jogadores[id]), 200
+
+    
 # Remover jogador por id
-@app.route("/jogador/<int:id>", methods=['DELETE'])
+@app.route("/jogador/<string:id>", methods=['DELETE'])
 def remover_jogador(id):
     jogador_removido = False
     
-    for jogador in jogadores:
+    for jogador in jogadores.values():
         if jogador["id"] == id:
             jogador_removido = True
-            jogadores.remove(jogador)
+            jogadores.pop(id)
             return jsonify(Mensagem = "Jogador removido com sucesso"), 200
         
     if not jogador_removido:
-        return jsonify(Mensagem = "Jogador não encontrado"), 404
-    
-# Remover jogador por nome
-@app.route("/jogador", methods=['DELETE'])
-def remover_jogador_nome():
-    nome = request.args.get("nome")
-    jogador_removido = False
-    
-    if not nome:
-        return jsonify(Mensagem = "Erro"), 400
-    
-    for jogador in jogadores:
-        if jogador["nome"] == nome:
-            jogador_removido = True
-            jogadores.remove(jogador)
-            return jsonify(Mensagem = "Jogador removido com sucesso"), 200
-        
-    if not jogador_removido:
-        return jsonify(Mensagem = "Jogador não encontrado"), 404          
+        abort(404, message="Jogador não encontrado")   
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
