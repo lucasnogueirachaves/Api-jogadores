db1940f546d8c4b66717dd46ea94a9d8f34c9ddd
Lucas Nogueira <lucasnogueirachaves@id.uff.br>
2026-01-06 16:20:50 -0300

    Adicionando db

---
+++ db.py
@@ -0,0 +1,16 @@
+jogadores = {
+    "1": {
+        "nome": "Lionel Messi",
+        "idade": 37,
+        "valor": 35000000,
+        "aposentado": False,
+        "time": "Inter Miami"
+    },
+    "2": {
+        "nome": "Cristiano Ronaldo",
+        "idade": 39,
+        "valor": 15000000,
+        "aposentado": False,
+        "time": "Al-Nassr"
+    }
+}
