5216e425d15ccdd2429b11ef7b5c4d7296949130
Lucas Nogueira <lucasnogueirachaves@id.uff.br>
2026-01-20 14:28:12 -0300

    Alterando estrutura do projeto e configurando app

--- app.py
+++ app.py
@@ -1,100 +1,21 @@
-from flask import Flask, request, jsonify
-from db import jogadores
-from flask_smorest import abort
-import uuid
+from flask import Flask
+from flask_smorest import Api
+from resource.jogador import jogador_blp
+from resource.time import time_blp
 
 app = Flask(__name__)
-app.json.sort_keys = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Minha API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https:_/cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-# GET da lista
-@app.route("/jogadores", methods=['GET'])
-def get_jogadores():
-    return jsonify(Jogadores = list(jogadores.values()))
+api = Api(app)
 
-# Busca jogador por id
-@app.route("/jogador/<string:id>", methods=['GET'])
-def get_jogador_id(id):
-    try:
-        return jsonify(jogador = jogadores[id]), 200
-    except:
-        abort(404, message="Jogador não encontrado")
-
-# Busca jogador por nome
-@app.route("/jogador", methods=['GET'])
-def get_jogador_nome():
-    nome = request.args.get("nome")
-    
-    for jogador in jogadores.values():
-        if jogador["nome"] == nome:
-            return jsonify(Mensagem = "Jogador encontrado", Jogador = jogador), 200
-        
-    abort(404, message="Jogador não encontrado")
-
-# Cadastro de jogador
-@app.route("/jogador", methods=['POST'])
-def cadastrar_jogador():
-    dado_jogador = request.get_json()
-    jogador_id = uuid.uuid4().hex
-    
-    jogador_novo = {**dado_jogador, "id": jogador_id}
-    
-    jogadores[jogador_id] = jogador_novo
-    
-    return jsonify(Mensagem = "Jogador adicionado"), 201
-    
-
-# Atualizar jogador
-@app.route("/jogador/<string:id>", methods=['PATCH'])
-def atualizar_jogador(id):
-    dado = request.get_json()
-    
-    if not dado:
-        abort(400, message="Dados inválidos")
-    
-    jogador = jogadores.get(id)
-
-    if not jogador:
-        abort(404, message="Jogador não encontrado")
-
-    jogador.update(dado)
-    
-    return jsonify(Mensagem = "Jogador atualizado com sucesso", Jogador = jogador), 200
-
-# Atualização completa
-@app.route("/jogador/<string:id>", methods=['PUT'])
-def atualizar_completo(id):
-    dado = request.get_json()
-
-    campos_obrigatorios = ["nome", "valor", "idade", "aposentado", "time"]
-
-    for campo in campos_obrigatorios:
-        if campo not in dado:
-            abort(400, message=f"Campo {campo} é obrigatório")
-
-    if id not in jogadores:
-        abort(404, message="Jogador não encontrado")
-
-    jogadores[id] = {
-        "id": id,
-        **dado
-    }
-
-    return jsonify(Mensagem="Jogador atualizado com sucesso", Jogador=jogadores[id]), 200
-
-    
-# Remover jogador por id
-@app.route("/jogador/<string:id>", methods=['DELETE'])
-def remover_jogador(id):
-    jogador_removido = False
-    
-    for jogador in jogadores.values():
-        if jogador["id"] == id:
-            jogador_removido = True
-            jogadores.pop(id)
-            return jsonify(Mensagem = "Jogador removido com sucesso"), 200
-        
-    if not jogador_removido:
-        abort(404, message="Jogador não encontrado")   
+api.register_blueprint(jogador_blp)
+api.register_blueprint(time_blp)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,16 +1,2 @@
-jogadores = {
-    "1": {
-        "nome": "Lionel Messi",
-        "idade": 37,
-        "valor": 35000000,
-        "aposentado": False,
-        "time": "Inter Miami"
-    },
-    "2": {
-        "nome": "Cristiano Ronaldo",
-        "idade": 39,
-        "valor": 15000000,
-        "aposentado": False,
-        "time": "Al-Nassr"
-    }
-}
+jogadores = {}
+times = {}
